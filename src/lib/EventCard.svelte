6eaf31639d061a4ccfe44a8558bee7a748ac54fd
Pete Blumenthal <pete@blumenthal.com>
2025-09-05 15:17:43 -0400

    remove another error being logged

--- src/lib/EventCard.svelte
+++ src/lib/EventCard.svelte
@@ -168,12 +168,4 @@
     -webkit-box-orient: vertical;
     overflow: hidden;
   }
-
-  .line-clamp-3 {
-    display: -webkit-box;
-    -webkit-line-clamp: 3;
-    line-clamp: 3;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-  }
 </style>
